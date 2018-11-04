ac5b59687e7680d71c11ffa8b70674ee4b6e2bbc
Bogdan Stancescu <bogdan.stancescu@rbc.com.ro>
2018-11-04 19:31:05 +0200

    Finished manual sweeps

--- DSLR Digitizer/SweepSettings.cs
+++ DSLR Digitizer/SweepSettings.cs
@@ -12,20 +12,21 @@ namespace DSLR_Digitizer
 
         public Point GetSweepSize()
         {
-            Point result = new Point();
-
-            result.X = (int)Math.Ceiling((double)SweepSize.X / DslrSize.X);
-            if (result.X > 1)
+            return new Point()
             {
-                result.X++;
-            }
+                X=ComputeSize(DslrSize.X, SweepSize.X),
+                Y=ComputeSize(DslrSize.Y, SweepSize.Y),
+            };
+        }
 
-            result.Y = (int)Math.Ceiling((double)SweepSize.Y / DslrSize.Y);
-            if (result.Y > 1)
-            {
-                result.Y++;
-            }
-            return result;
+        /*
+         * We subtract a unit because we want to make sure floor(3/1) == 2,
+         * which sounds messed up, but it's the only way I could find to include
+         * the last segment.
+         */
+        private int ComputeSize(int dslrSize, int sweepSize)
+        {
+            return (int)Math.Floor(((double)sweepSize - 1) / dslrSize) + 1;
         }
     }
 
